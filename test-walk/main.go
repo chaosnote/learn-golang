b30d2ac5a18661c56695babe730d31fe83a9ed75
chris <yeditor@gmail.com>
2025-10-19 12:05:05 +0800

    增加內容輸出

--- test-walk/main.go
+++ test-walk/main.go
@@ -7,6 +7,28 @@ import (
 	"path/filepath"
 )
 
+const (
+	file_path = "./dist/output.txt"
+)
+
+func write_file(content []byte) {
+	// 1. 使用 os.OpenFile 開啟檔案，設定 O_APPEND, O_CREATE, O_WRONLY
+	// O_APPEND: 追加模式
+	// O_CREATE: 如果檔案不存在則建立
+	// O_WRONLY: 唯寫模式
+	file, err := os.OpenFile(file_path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("無法開啟檔案: %v", err))
+	}
+	defer file.Close() // 確保在函式結束時關閉檔案
+
+	// 2. 寫入數據
+	_, err = file.Write(content)
+	if err != nil {
+		panic(fmt.Sprintf("寫入檔案失敗: %v", err))
+	}
+}
+
 func PrintTree(path string, prefix string) {
 	entries, err := os.ReadDir(path)
 	if err != nil {
@@ -20,7 +42,8 @@ func PrintTree(path string, prefix string) {
 			connector = "└── "
 		}
 
-		fmt.Println(prefix + connector + entry.Name())
+		output_path := prefix + connector + entry.Name() + "\n"
+		write_file([]byte(output_path))
 
 		if entry.IsDir() {
 			subPrefix := prefix
@@ -30,6 +53,12 @@ func PrintTree(path string, prefix string) {
 				subPrefix += "│   "
 			}
 			PrintTree(filepath.Join(path, entry.Name()), subPrefix)
+		} else if filepath.Ext(entry.Name()) == ".go" {
+			content, err := os.ReadFile(filepath.Join(path, entry.Name()))
+			if err != nil {
+				panic(fmt.Sprintf("寫入檔案失敗: %v", err))
+			}
+			write_file(content)
 		}
 	}
 }
@@ -45,5 +74,8 @@ func main() {
 
 	pwd = *dir_path
 	fmt.Println(pwd)
+
+	os.WriteFile(file_path, []byte(""), 0777)
+
 	PrintTree(pwd, "")
 }
